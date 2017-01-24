1e1cb4e15108807b853dc56fd429340cf1b6568c
Joseph Nix <nixjdm@terminallabs.com>
2017-01-24 17:07:25 -0600

    Cleaning clients html and js. Adding client content back in.

--- assets/static/js/slideshow_events.js
+++ assets/static/js/slideshow_events.js
@@ -1,6 +1,4 @@
-function hide_clients()
-{
-  console.log("in hide_clients");
+function hide_clients() {
   $.each($(".client_slide"), function(index, value) {
     $(value).hide();
   });
@@ -9,21 +7,17 @@ function hide_clients()
   });
 }
 
-function load_modavanti()
-{
-  console.log("in load modavanti");
+function load_modavanti() {
   hide_clients();
   $("#modavanti_underline").show();
   $("#client_modavanti").show();
 }
-function init_slideshow_events()
-{
-  console.log("in init_slideshow_events");
-  $('.menu_item').click(function(e)
-    {
-      hide_clients();
-      var id = this.id;
-      $("#" + id + "_underline").show();
-      $("#client_" + id).show();
-    });
+
+function init_slideshow_events() {
+  $('.menu_item').click(function(e) {
+    hide_clients();
+    var id = this.id;
+    $("#" + id + "_underline").show();
+    $("#client_" + id).show();
+  });
 }
