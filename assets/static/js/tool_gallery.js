93cbaa9148ddf81e82477d3edf8532cb27fa3162
Joseph Nix <nixjdm@terminallabs.com>
2017-02-03 12:24:43 -0600

    [tl-lektor-6] Adding in tech tree animation and reindenting some js files.

--- assets/static/js/tool_gallery.js
+++ assets/static/js/tool_gallery.js
@@ -1,18 +1,18 @@
 function hide_slides()
 {
-    $.each($(".gallery_tool_text"), function(index, value) {
-        $(value).hide();
-    });
+  $.each($(".gallery_tool_text"), function(index, value) {
+    $(value).hide();
+  });
 }
 
 
 $( document ).ready(function() {
+  hide_slides();
+  $(".slide").hide()
+  $(".gallery_tool").click(function() {
+    var id = this.id;
     hide_slides();
-    $(".slide").hide()
-    $(".gallery_tool").click(function() {
-        var id = this.id;
-        hide_slides();
-        $(".large_tech_text_upper").html($("#" + id).children(".gallery_tool_text").html());
-        $(".slide").show()
-    });
+    $(".large_tech_text_upper").html($("#" + id).children(".gallery_tool_text").html());
+    $(".slide").show()
+  });
 });
